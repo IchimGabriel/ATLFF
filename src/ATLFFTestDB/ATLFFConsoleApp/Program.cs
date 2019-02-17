6deec9fda176a2ad3e51457a39ec2c7366c4d3ac
Gabriel Ichim <ichimgabriel75@gmail.com>
2019-02-17 01:20:51 +0000

    Create Console App to test DB
    neo4J - Graph Database -test connection

---
+++ src/ATLFFTestDB/ATLFFConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATLFFConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
