4644b983ae6060f6f292b50a0b4c6a28c2946f42
Gabriel Ichim <ichimgabriel75@gmail.com>
2019-02-17 02:10:21 +0000

    Model - City , Edge
    City (node) - Edge (route seqment)

---
+++ src/ATLFFTestDB/ATLFF.Core/Model/City.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATLFF.Core.Model
+{
+    class City
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("lat")]
+        public float Latitude { get; set; }
+        [JsonProperty("lng")]
+        public float Longitude { get; set; }
+        [JsonProperty("iso")]
+        public string Country { get; set; }     // iso3 - IRL . GBR
+        [JsonProperty("port_city")]
+        public bool Port { get; set; }          // true or false
+        [JsonProperty("turnaround")]
+        public int Turnaround { get; set; }     // overhead time - hours
+    }
+}
---
+++ src/ATLFFTestDB/ATLFF.Core/Model/Edge.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATLFF.Core.Model
+{
+    class Edge
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }    // TRUCK . TRAIN . SHIP . BARGE
+        [JsonProperty("distance")]
+        public int Distance { get; set; }   // Km
+
+        public int Speed { get; set; }      // Km/h
+
+        public float Emission { get; set; } // CO2 emission
+    }
+}
